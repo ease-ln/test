1a197f0cf06e28a1f23da03da5b5281c6592483a
ease-ln <69844629+ease-ln@users.noreply.github.com>
2022-06-01 02:28:42 +0300

    init

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 
   const response = await fetch(request);
   const superHeroes = await response.json();
+
+  var json = JSON.stringify({ table : [] });
+  var fs = require('fs');
+  fs.writeFile('./data.json', json, 'utf8', callback);
