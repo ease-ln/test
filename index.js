0c018cee965371f4710772ace0765769ca843cda
ease-ln <poiuyt171819@gmail.com>
2022-07-04 12:40:48 +0300

    main

--- index.js
+++
@@ -1,5 +0,0 @@
-const requestURL = ' https://ease-ln.github.io/test/data.json';
-const request = new Request(requestURL);
-
-const response = await fetch(request);
-const data = await response.json();
--- src/index.js
+++
@@ -1,9 +0,0 @@
-  const requestURL = ' https://ease-ln.github.io/test/data.json';
-  const request = new Request(requestURL);
-
-  const response = await fetch(request);
-  const superHeroes = await response.json();
-
-  var json = JSON.stringify({ table : [] });
-  var fs = require('fs');
-  fs.writeFile('./data.json', json, 'utf8', callback);
